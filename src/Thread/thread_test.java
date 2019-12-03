55354d8d5ea396f71dec618e1ca43602d6d9cddb
WANGYISU <asueeer@163.com>
2019-12-03 21:44:00 +0800

    add thread

---
+++ src/Thread/thread_test.java
@@ -0,0 +1,55 @@
+package Thread;
+
+public class thread_test {
+    private static final long count = 100000000;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrent();
+        serial();
+    }
+
+
+    private static void concurrent() throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (int i = 0; i < count; i++) {
+                    a = a+5;
+                }
+            }
+        });
+
+        thread.start();
+
+        int b = 0;
+        for (int i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+
+        long time = System.currentTimeMillis() - start;
+        System.out.println("多线程所用时间：" + time + "ms, b = ");
+    }
+
+
+    private static void serial(){
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (int i = 0; i < count; i++) {
+            a = a+5;
+        }
+
+        int b = 0;
+        for (int i = 0; i < count; i++) {
+            b--;
+        }
+
+        long time = System.currentTimeMillis() - start;
+
+        System.out.println("串行所用时间：" + time + "ms, b = ");
+
+    }
+}
