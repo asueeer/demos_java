eef4a857e85d3a31b01142f1f7d64936b58e1234
WANGYISU <asueeer@163.com>
2020-01-31 16:15:38 +0800

    add thread

--- src/Solution/LRU_Cache.java
+++ src/Solution/LRU_Cache.java
@@ -1,7 +1,20 @@
 package Solution;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.HashMap;
 
 public class LRU_Cache {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n= Integer.valueOf(br.readLine());
+        String s = null;
+        LinkedList<Integer> list = new LinkedList();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        while (!((s=br.readLine())==null))
+        {
+
+        }
+    }
 }
